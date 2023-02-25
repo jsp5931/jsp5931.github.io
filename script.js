4374f4a7dc701c0b5cf02a7cea4a80f801f910d2
Jaspreet <jaspreetparhar@gmail.com>
2023-02-25 15:23:24 -0500

    v7

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ function addsupertask(){
     icon.classList.add('icon','d-flex','justify-content-center');
     //https://stackoverflow.com/questions/9618504/how-to-get-the-selected-radio-button-s-value
     var task_type_input = document.querySelector('input[name="type"]:checked').value;
-    console.log(task_type_input)
+
     
     if(task_type_input == "School"){
         icon.innerHTML =`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book" viewBox="0 0 16 16">
@@ -92,21 +92,44 @@ function addsupertask(){
 
     var buttons = document.createElement('span');
     buttons.classList.add('action-buttons');
-    buttons.innerHTML = `<div class="btn-group" role="group">
-    <button type="button" class="btn btn-secondary btn-sm action-btn"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book" viewBox="0 0 16 16"><path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/></svg>
-    </button>
-    <button type="button" class="btn btn-primary btn-sm action-btn"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16"><path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/></svg>
-  </button>
-    <button type="button" class="btn btn-danger btn-sm action-btn"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/><path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2h4a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/></svg>
-  </button>
-  </div>`;
+    var btn_div = document.createElement('div');
+    btn_div.classList.add('btn-group');
+    btn_div.setAttribute('role','group');
 
+    var edit = document.createElement('button');
+    edit.classList.add('btn', 'btn-info', 'btn-sm', 'action-btn','edit-btn');
+    edit.setAttribute('type','button');
+    edit.setAttribute('title',"Edit task");
+    edit.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16"><path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
+    </svg>`;
+
+    var addsubtask = document.createElement('button');
+    addsubtask.classList.add('btn', 'btn-success', 'btn-sm', 'action-btn','addsubtask-btn');
+    addsubtask.setAttribute('type','button');
+    addsubtask.setAttribute('title','Add subtask');
+    addsubtask.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+  </svg>`;
+
+  var deletebtn = document.createElement('button');
+  deletebtn.classList.add('btn', 'btn-danger', 'btn-sm', 'action-btn','delete-btn');
+  deletebtn.setAttribute('type','button');
+  deletebtn.setAttribute('title', 'Delete Task')
+  deletebtn.setAttribute('id','delete-button'+(numitems+1));
+  deletebtn.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/><path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2h4a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+  </svg>`;
+    btn_div.appendChild(edit);
+    btn_div.appendChild(addsubtask);
+    btn_div.appendChild(deletebtn);
+    buttons.appendChild(btn_div);
 
     task_type.appendChild(task_type_content);
     task_type.appendChild(icon);
     supertask.appendChild(task_type);
     supertask.appendChild(buttons);
     li.appendChild(supertask);
+    li.setAttribute('id', 'item'+(numitems+1));
     task_list.appendChild(li);
     task_form.reset();
 
@@ -158,8 +181,8 @@ function addsupertask(){
         task_icon.classList.remove('checked');
         for(var k=0;k<subtasks.length;k++){
           subtasks[k].getElementsByClassName('form-check-input')[0].removeAttribute("disabled");
-          subtasks[k].getElementsByClassName('form-check-input')[0].checked=false;
-          subtasks[k].getElementsByClassName('nomen')[0].classList.remove('checked');
+          // subtasks[k].getElementsByClassName('form-check-input')[0].checked=false;
+          // subtasks[k].getElementsByClassName('nomen')[0].classList.remove('checked');
         }
 
       }
@@ -170,7 +193,14 @@ function addsupertask(){
     checkboxes[i].addEventListener('change',handlecheck);
   }
 
-
+  var list_container = document.querySelector('.list-container');
+  
+  https://stackoverflow.com/questions/59506089/creating-a-delete-button-in-javascript-to-remove-shopping-list-items
+  list_container.addEventListener('click', (e) => {
+    if(e.target.classList.contains('delete-btn')){
+        e.target.closest("li").remove();
+  }}
+  )
   
 
   
